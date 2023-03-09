a80b781a0388283f385b456bbf3a0c90c34ed877
d1av <davi4alves@gmail.com>
2023-03-09 15:41:20 -0300

    feat: :sparkles: add enterprise total amount after comission

--- src/testdevoperacional/application/EmpresaUseCase.java
+++ src/testdevoperacional/application/EmpresaUseCase.java
@@ -12,6 +12,8 @@ import testdevoperacional.domain.Usuario;
 import testdevoperacional.domain.Venda;
 
 public class EmpresaUseCase {
+
+    private static Double SALDO_TOTAL_APOS_COMISSAO = 0d;
     
     public static void verProdutosEmpresa(List<Produto> produtos,
 	    Usuario usuarioLogado) {
@@ -21,11 +23,11 @@ public class EmpresaUseCase {
 	produtos.stream().forEach(produto -> {
 	    if (produto.getEmpresa().getId()
 		    .equals(usuarioLogado.getEmpresa().getId())) {
-		
+
 		printarBarra();
-		
+
 		printarProduto(produto);
-		
+
 		printarBarra();
 	    }
 
@@ -35,9 +37,10 @@ public class EmpresaUseCase {
 	printarBarra();
     }
 
+
     public static void listarVendasEmpresa(List<Venda> vendas,
 	    Usuario usuarioLogado) {
-	
+	SALDO_TOTAL_APOS_COMISSAO = 0d;
 	pularLinha();
 	printarBarra();
 	System.out.println("VENDAS EFETUADAS");
@@ -47,22 +50,21 @@ public class EmpresaUseCase {
 		printarBarra();
 		System.out.println("Venda de código: " + venda.getCódigo()
 			+ " no CPF " + venda.getCliente().getCpf() + ": ");
-		
+
 		printarVendas(venda);
-			
-		
+
 		System.out.println("Total Venda: R$" + venda.getValor());
 		System.out.println("Total Taxa a ser paga: R$"
 			+ venda.getComissaoSistema());
 		System.out.println("Total Líquido  para empresa: R$"
 			+ (venda.getValor() - venda.getComissaoSistema()));
 		printarBarra();
-	    }
 
+		SALDO_TOTAL_APOS_COMISSAO += (venda.getValor() - venda.getComissaoSistema());
+	    }
 	});
-	
-	System.out.println(
-		"Saldo Empresa: " + usuarioLogado.getEmpresa().getSaldo());
+
+	System.out.println("Saldo Empresa: R$" + SALDO_TOTAL_APOS_COMISSAO);
 	printarBarra();
     }
 }
